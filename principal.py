9bf18db6b167a5a1248415c0aec1b38dab0af902
juanmaalv <47435120+juanmaalv@users.noreply.github.com>
2020-03-12 09:22:04 -0600

    Add files via upload

--- principal.py
+++ principal.py
@@ -1,18 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 26 20:41:32 2020
-
-@author: juanm
-"""
-
 # -- ------------------------------------------------------------------------------------ -- #
 # -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
-# -- archivo: principal.py 
-# -- mantiene: Juan Manuel ACC
-# -- repositorio: https://github.com/juanmaalv/LAB_2_JMACC
+# -- archivo: principal.py - flujo principal del proyecto
+# -- mantiene: mauanaya
+# -- repositorio: https://github.com/mauanaya/LAB_2_VMAA
 # -- ------------------------------------------------------------------------------------ -- #
 
 import funciones as fn
-
+#%%
 datos = fn.f_leer_archivo(param_archivo='archivo_tradeview_1.xlsx')
-
+fn.f_pip_size(param_ins='audusd')
+datos = fn.f_columnas_tiempo(param_data=datos)
+datos = fn.f_columnas_pips(param_data=datos)
